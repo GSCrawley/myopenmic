c25f5f9b429fbeb94ebece9a11a4efbd673cd80a
GSCrawley <gscrawley@gmail.com>
2019-10-16 10:38:44 -0700

    setting port

--- app.py
+++ app.py
@@ -88,4 +88,4 @@ def comments_delete(comment_id):
     return redirect(url_for('videos_show', video_id=comment.get('video_id')))
 
 if __name__ == '__main__':
-        app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
+  app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
