e32a6fcce78966132ef55eb381a9e0aa105ce6c4
GSCrawley <gscrawley@gmail.com>
2019-10-15 20:42:00 -0700

    added mongodb

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.myopenmic
+videos = db.videos
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# OUR MOCK ARRAY OF PROJECTS
-playlists = [
-    { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
-    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
-]
+# # OUR MOCK ARRAY OF PROJECTS
+# videos = [
+#     { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
+#     { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
+# ]
+
 
 @app.route('/')
-def playlists_index():
-    """Show all playlists."""
-    return render_template('playlists_index.html', playlists=playlists)
+def videos_index():
+    """Show all videos."""
+    return render_template('videos_index.html', videos=videos.find())
+
+
+@app.route('/videos/new')
+def videos_new():
+    """Create a new video."""
+    return render_template('videos_new.html', video={}, title='New video')
